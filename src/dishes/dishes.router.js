46ee99d08572dc439a77c248ebe3cd9ce0a6b084
Jeremiah Bautista <jeremiah.bautista@gmail.com>
2024-02-16 16:46:53 -0800

    feat(dishes): guards against delete verb

--- src/dishes/dishes.router.js
+++ src/dishes/dishes.router.js
@@ -1,8 +1,13 @@
 const router = require("express").Router();
+const methodNotAllowed = require("../../../starter-robust-server-structure-paste/src/errors/methodNotAllowed");
 const controller = require("./dishes.controller");
 
 // TODO: Implement the /dishes routes needed to make the tests pass
-router.route("/:dishId").get(controller.read).put(controller.update);
+router
+  .route("/:dishId")
+  .get(controller.read)
+  .put(controller.update)
+  .all(methodNotAllowed);
 
 router.route("/").get(controller.list).post(controller.create);
 
