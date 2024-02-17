afdfb9c9ad7023591aa9fbddf8478e96e0fb3c4d
Jeremiah Bautista <jeremiah.bautista@gmail.com>
2024-02-16 16:19:20 -0800

    feat(dishes): adds create route and dish validation

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -11,6 +11,60 @@ const list = (req, res) => {
   res.json({ data: dishes });
 };
 
+const validateDish = (field) => {
+  return (req, res, next) => {
+    const fieldValue = req.body.data[field];
+
+    switch (field) {
+      case "description":
+      case "name":
+      case "image_url":
+        if (!fieldValue || fieldValue.length === 0) {
+          return next({
+            status: 400,
+            message: `please provide a value for ${field}`,
+          });
+        }
+        return next();
+      case "price":
+        // implicitly checks presence of price
+        if (typeof fieldValue !== "number") {
+          return next({
+            status: 400,
+            message: `price must be a number`,
+          });
+        }
+        if (fieldValue <= 0) {
+          return next({
+            status: 400,
+            message: `price must be more than 0`,
+          });
+        }
+        return next();
+      default:
+        return next({
+          status: 400,
+          message: "cannot validate unrecognized field",
+        });
+    }
+  };
+};
+
+const create = (req, res, next) => {
+  const newDish = { ...req.body.data, id: nextId() };
+
+  dishes.push(newDish);
+
+  res.status(201).json({ data: newDish });
+};
+
 module.exports = {
+  create: [
+    validateDish("description"),
+    validateDish("name"),
+    validateDish("image_url"),
+    validateDish("price"),
+    create,
+  ],
   list,
 };
